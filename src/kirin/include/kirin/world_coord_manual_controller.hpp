188710d3a3def82dcda9d0ebcf1039bae02506af
Kenji Fukuda <fk.kenji.21@gmail.com>
2022-08-27 21:38:12 +0900

    add ik calculation code

--- src/kirin/include/kirin/world_coord_manual_controller.hpp
+++ src/kirin/include/kirin/world_coord_manual_controller.hpp
@@ -5,7 +5,9 @@
 #include <Eigen/Core>
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 #include "kirin/joy_controller.hpp"
+
 
 class WorldCoordManualController : public JoyController {
  public:
@@ -17,12 +19,18 @@ class WorldCoordManualController : public JoyController {
   const std::string input_topic_name_{"world_coord_pose"};
   std::function<void()> timer_callback_;
   Eigen::Vector3d pos;
-  double theta;
+  double psi;
+  int ik_index{0};
 
   rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr world_coord_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_pub_;
   rclcpp::TimerBase::SharedPtr timer_;
 
   void TimerCallback();
+  double CalcR(double l, double x, double y, double psi);
+  double CalcPhi(double l, double x, double y, double psi);
+  double CalcTheta(double l, double x, double y, double psi);
+  
 };
 
 
--- src/kirin/src/world_coord_manual_controller.cpp
+++ src/kirin/src/world_coord_manual_controller.cpp
@@ -1,15 +1,19 @@
 #include <Eigen/Geometry>
 #include <tf2_eigen/tf2_eigen.h>
 #include "kirin/world_coord_manual_controller.hpp"
+#include "ik_r.h"
+#include "ik_phi.h"
+#include "ik_theta.h"
 
 WorldCoordManualController::WorldCoordManualController(const std::string& node_name)
   : JoyController(node_name, "/joy"),
-    pos(0.5, 0.5, 1.0),
-    theta(0.0),
+    pos(0.3, 0.3, 0.6),
+    psi(0.0),
     timer_callback_(std::bind(&WorldCoordManualController::TimerCallback, this)){
   using namespace std::chrono_literals;
   rclcpp::QoS qos(rclcpp::KeepLast(10));
   world_coord_pub_ = create_publisher<geometry_msgs::msg::PoseStamped>(input_topic_name_, qos);
+  joint_pub_ = create_publisher<sensor_msgs::msg::JointState>("manual_joint", qos);
   timer_ = create_wall_timer(10ms, timer_callback_);
 }
 
@@ -32,9 +36,40 @@ void WorldCoordManualController::TimerCallback() {
   double left_trig = 1.0 - this->GetAxis(JoyController::Axis::LTrigger);
   double right_trig = 1.0 - this->GetAxis(JoyController::Axis::RTrigger);
   double omega = (left_trig - right_trig) * 0.2;
-  theta += omega*0.01;
-  Eigen::Quaterniond quat(Eigen::AngleAxisd(theta, Eigen::Vector3d::UnitZ()));
+  psi += omega*0.01;
+  Eigen::Quaterniond quat(Eigen::AngleAxisd(psi, Eigen::Vector3d::UnitZ()));
   input_pose->pose.orientation = Eigen::toMsg(quat);
 
   world_coord_pub_->publish(std::move(input_pose));
+
+  auto joint_state = std::make_unique<sensor_msgs::msg::JointState>();
+  joint_state->name = {
+    "theta_joint", "z_joint", "r_joint", "phi_joint", "phi_extend_joint"
+  };
+  double l = 0.05;
+  double r = CalcR(l, pos.x(), pos.y(), psi);
+  double theta = CalcTheta(l, pos.x(), pos.y(), psi);
+  double phi = CalcPhi(l, pos.x(), pos.y(), psi);
+  joint_state->position = {
+    theta, pos.z(), r, phi, phi 
+  };
+  joint_pub_->publish(std::move(joint_state));
+}
+
+double WorldCoordManualController::CalcR(double l, double x, double y, double psi) {
+  double out[2];
+  model::ik_r(l, psi, x, y, out);
+  return out[ik_index];
+}
+
+double WorldCoordManualController::CalcPhi(double l, double x, double y, double psi) {
+  double out[2];
+  model::ik_phi(l, psi, x, y, out);
+  return out[ik_index];
+}
+
+double WorldCoordManualController::CalcTheta(double l, double x, double y, double psi) {
+  double out[2];
+  model::ik_theta(l, psi, x, y, out);
+  return out[ik_index];
 }
