2db96d60ed1e6ffd094491646c169fd04ea9db03
Kenji Fukuda <fk.kenji.21@gmail.com>
2022-09-07 20:40:17 +0900

    add air state common types and msg

--- src/kirin/include/kirin/common_types.hpp
+++ src/kirin/include/kirin/common_types.hpp
@@ -1,6 +1,8 @@
 #ifndef SRC_CATCHROBO_SRC_KIRIN_INCLUDE_KIRIN_COMMON_TYPES
 #define SRC_CATCHROBO_SRC_KIRIN_INCLUDE_KIRIN_COMMON_TYPES
 
+
+
 struct Joint {
   double theta; // [rad]
   double z;     // [m]
@@ -14,5 +16,20 @@ struct MotorAngle { // [rad]
   double right;
   double z;
 };
+
+namespace kirin_type {
+  enum class BellowsName {
+    Top,
+    Left,
+    Right,
+    ExLeft,
+    ExRight
+  };
+
+  enum class AirState {
+    On = 1,
+    Off = 0
+  };
+}
 
 #endif /* SRC_CATCHROBO_SRC_KIRIN_INCLUDE_KIRIN_COMMON_TYPES */
