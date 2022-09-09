673b625da5f650832ed222d6bc76e887ce24d669
Kenji Fukuda <fk.kenji.21@gmail.com>
2022-09-09 15:17:59 +0900

    add bellows parameter in machine.hpp

--- src/kirin/include/kirin/machine.hpp
+++ src/kirin/include/kirin/machine.hpp
@@ -1,29 +1,30 @@
 #ifndef SRC_CATCHROBO_SRC_KIRIN_INCLUDE_KIRIN_MACHINE
 #define SRC_CATCHROBO_SRC_KIRIN_INCLUDE_KIRIN_MACHINE
 
+#include <math.h>
+
 // TODO: obtain mathine params from URDF file
 namespace machine {
-// TODO: make temp true parameter
 /* phi rotation radius */
-constexpr double kPhiRadius = 0.03979;
+constexpr double kPhiRadius      = 0.03979;
 /* motor rotation radius */
-constexpr double kMotorRadius = 0.023875;
+constexpr double kMotorRadius    = 0.023875;
 /* theta gear ratio */
 constexpr double kThetaGearRatio = 25. / 136.;
 // /* z gear ratio */
 // constexpr double kZGearRatio = 1.0; // (temp)
 // /* motor gear radius */
 // constexpr double kZGearRadius = 1.0; // (temp)
-constexpr double kZRatio = 0.02387 / 2 * M_PI;
+constexpr double kZRatio         = 0.02387 / 2. * M_PI;
 
 /* z offset from base axis to theta link */
-constexpr double kZOffsetBaseToTheta = 0.022;
+constexpr double kZOffsetBaseToTheta  = 0.022;
 /* z offset from theta axis to z link */
-constexpr double kZOffsetThetaToZ = 0.128;
+constexpr double kZOffsetThetaToZ     = 0.128;
 /* r offset from center axis to root of r link */
 constexpr double kOffsetCenterToRRoot = 0.201;
 /* r offset from r root to phi link */
-constexpr double kOffsetRRootToPhi = 0.345;
+constexpr double kOffsetRRootToPhi    = 0.345;
 
 /* joint state limit */
 namespace limit {
@@ -34,6 +35,15 @@ constexpr double kZUpper = 0.15;
 constexpr double kRLower = 0.0;
 constexpr double kRUpper = 0.413;
 }  // namespace limit
+
+/* bellows position parameter */
+namespace bellows {
+constexpr double kZ        = -0.042;
+constexpr double kTop_X    = 0.05;
+constexpr double kShrink_D = 0.10;
+constexpr double kExtend_D = 0.14;
+}  // namespace bellows
+
 }  // namespace machine
 
 #endif /* SRC_CATCHROBO_SRC_KIRIN_INCLUDE_KIRIN_MACHINE */
