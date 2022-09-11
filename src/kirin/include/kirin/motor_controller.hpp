f03509bb88522789c03dfd2b57470f7193338c0e
Y.Hisaki <yhisaki31@gmail.com>
2022-09-11 20:53:33 +0900

    update

--- src/kirin/include/kirin/motor_controller.hpp
+++ src/kirin/include/kirin/motor_controller.hpp
@@ -2,24 +2,44 @@
 #define SRC_CATCHROBO_SRC_KIRIN_INCLUDE_KIRIN_MOTOR_CONTROLLER
 
 #include <kirin_msgs/msg/motor_state_vector.hpp>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
 
 #include "ddt-motor/angle_filter.hpp"
 #include "ddt-motor/motor.hpp"
 #include "ddt-motor/uart.hpp"
 
+struct ControllerBase {
+  ddt::AngleFilter filter;
+  std::optional<ddt::Motor::State> state = std::nullopt;
+  double angle = 0;
+  double velocity = 0;
+  virtual double GetInput(double ref_velocity, double ref_angle);
+  void Update(std::optional<ddt::Motor::State> state);
+};
+
+struct ControllerVelocityInput : public ControllerBase {
+  int dir = 1;
+  double Kp;
+  double max_speed;
+  ControllerVelocityInput(int dir, double Kp, double max_speed);
+  double GetInput(double ref_velocity, double ref_angle);
+};
+
+struct ControllerCurrentInput {};
+
 class MotorController : public rclcpp::Node {
  public:
   using MotorStateVector = kirin_msgs::msg::MotorStateVector;
-  explicit MotorController(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+  explicit MotorController(
+      const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
  private:
-  std::shared_ptr<ddt::Uart> uart;
-  ddt::AngleFilter angle_filter_right, angle_filter_left;
-  ddt::Motor motor_right, motor_left;
-  double max_speed, kp, ki, kd;
-  double motor_right_input = 0.0;
-  double motor_left_input  = 0.0;
+  bool is_simulation;
+  std::chrono::milliseconds sleep_time;
+  std::optional<ddt::Motor> motor_right, motor_left, motor_theta, motor_z;
+  std::optional<ControllerVelocityInput> controller_right, controller_left,
+      controller_z;
 
   void MotorStateVectorReceiveCallback(const MotorStateVector::UniquePtr msg);
   std::function<void(const MotorStateVector::UniquePtr)> motor_callback_;
