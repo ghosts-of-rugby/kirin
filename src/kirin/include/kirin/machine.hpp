c1ff173c15c3ac988c84970221b2f43e74c3f7af
Kenji Fukuda <fk.kenji.21@gmail.com>
2022-09-16 03:37:10 +0900

    change machine parameter

--- src/kirin/include/kirin/machine.hpp
+++ src/kirin/include/kirin/machine.hpp
@@ -12,15 +12,15 @@
 #include "dtheta.h"
 #include "dphi.h"
 
-// TODO: obtain mathine params from URDF file
+// TODO: obtain mathine params from URDF files
 namespace machine {
 /* phi rotation radius */
 constexpr double kPhiRadius      = 0.03979;
 /* motor rotation radius */
 constexpr double kMotorRadius    = 0.023875;
 /* theta gear ratio */
-constexpr double kThetaGearRatio = 15. / 136.;
-// /* z gear ratio */
+constexpr double kThetaGearRatio = 25. / 136.;
+// /* z gear ratio *
 // constexpr double kZGearRatio = 1.0; // (temp)
 // /* motor gear radius */
 // constexpr double kZGearRadius = 1.0; // (temp)
